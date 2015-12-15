f74b2f13acf308875b1120a24423c77ab1a33f6e
k88hudson <k88hudson@gmail.com>
2015-12-14 20:27:40 -0500

    fix fake prefs

--- src/lib/platform.js
+++ src/lib/platform.js
@@ -1,11 +1,11 @@
 const FAKE_PREFS = new Map([
   ['browser.newtabpage.rows', 3],
   ['browser.newtabpage.columns', 5],
-  ['browser.newtabpage.enabled', true],
-  ['browser.newtabpage.enhanced', true],
+  ['browser.newtabpage.enabled', 'true'],
+  ['browser.newtabpage.enhanced', 'true'],
   ['browser.newtabpage.pinned', undefined],
-  ['browser.newtabpage.remote', false],
-  ['intl.locale.matchOS', false],
+  ['browser.newtabpage.remote', 'false'],
+  ['intl.locale.matchOS', 'false'],
   ['general.useragent.locale', 'en-US']
 ]);
 
